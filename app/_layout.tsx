439c12fb530bc850a605690c79489f3499e92ecc
Bunmeng Te <tebunmeng2004@gmail.com>
2026-05-07 16:02:46 +1000

    Initialize SQLite migrations on app startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
-import { Stack } from 'expo-router';
-import { ThemeProvider } from '../src/theme';
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+
+import { runMigrations } from "../src/database/migrations";
+import { ThemeProvider } from "../src/theme";
 
 export default function RootLayout() {
+  useEffect(() => {
+    runMigrations();
+  }, []);
+
   return (
     <ThemeProvider>
       <Stack
